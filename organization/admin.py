9f909f5da04772e6d3638fd14d43e030bb76eae9
Pratik <contact.pratik10@gmail.com>
2024-10-06 18:55:10 +0530

    Organization tables admin site

--- organization/admin.py
+++ organization/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from organization.models import *
 
-# Register your models here.
+admin.site.register(Organization)
+admin.site.register(OrganizationCategory)
+admin.site.register(OrganizationRole)
+admin.site.register(OrganizationMember)
